caa923c0e7f3a8a3071025db97a84f58e8ecfd34
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-02-24 00:26:25 +0300

    Create the Spring Boot development environment for the ForMyFuture project

---
+++ ForMyFuture/src/main/java/com/ieg/ForMyFuture/ForMyFutureApplication.java
@@ -0,0 +1,13 @@
+package com.ieg.ForMyFuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForMyFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMyFutureApplication.class, args);
+	}
+
+}
---
+++ ForMyFuture/src/test/java/com/ieg/ForMyFuture/ForMyFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieg.ForMyFuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForMyFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
